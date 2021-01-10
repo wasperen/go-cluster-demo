50be59b66f16e6ad9710b3333fbdc560ce4f7d72
Willem van Asperen <willem@van.asperen.org>
2021-01-10 10:40:13 +0100

    retrying to become member

--- cmd/cacher/main.go
+++ cmd/cacher/main.go
@@ -8,6 +8,22 @@ import (
 	"github.com/hashicorp/memberlist"
 )
 
+const retries uint8 = 15
+
+func becomeMember(list *memberlist.Memberlist, knownHosts []string) error {
+	fmt.Printf("joining list via known member(s) %s\n", knownHosts)
+
+	var err error
+	for retry := uint8(0); retry < retries; retry++ {
+		_, err = list.Join(knownHosts)
+		if err != nil {
+			return nil
+		}
+		time.Sleep(time.Second * 1)
+	}
+	return err
+}
+
 func main() {
 	list, err := memberlist.Create(memberlist.DefaultLocalConfig())
 	if err != nil {
@@ -15,8 +31,7 @@ func main() {
 	}
 
 	if len(os.Args[1:]) > 0 {
-		fmt.Printf("joining list via known member(s) %s\n", os.Args[1:])
-		_, err = list.Join(os.Args[1:])
+		err = becomeMember(list, os.Args[1:])
 		if err != nil {
 			panic("Failed to join cluster: " + err.Error())
 		}
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/wasperen/go-cluster-demo
 
 go 1.15
 
-require github.com/hashicorp/memberlist v0.2.2
+require (
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
+	github.com/hashicorp/memberlist v0.2.2
+	github.com/kr/pretty v0.2.0 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
+	golang.org/x/sys v0.0.0-20201112073958-5cba982894dd // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+)
